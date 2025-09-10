c8c74ec52d2da55b787abcaedfa9a5b00bae266b
Omkar Kabde <omkarkabde@gmail.com>
2025-09-11 03:52:26 +0530

    fix hf

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ huggingface-cli login
 # to use hf_transfer
 export HF_HUB_ENABLE_HF_TRANSFER=1
 
-huggingface-cli download omkar334/PIDdataset --repo-type dataset annotations.zip --local-dir .
-huggingface-cli download omkar334/PIDdataset --repo-type dataset images.zip --local-dir .
+huggingface-cli download omkar334/PIDdataset annotations.zip --repo-type dataset --local-dir .
+huggingface-cli download omkar334/PIDdataset images.zip --repo-type dataset --local-dir .
 
 unzip annotations.zip -d dataset > /dev/null 2>&1
 unzip images.zip -d dataset > /dev/null 2>&1
